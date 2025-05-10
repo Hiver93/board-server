af0096d3d90e952857cb86f32d661ad96f040b8a
kdw3238 <kdw3238@naver.com>
2025-05-10 21:52:23 +0900

    day95 - 1
    config, error, common 코드 작성

---
+++ board server/day095/Spring/board/src/main/java/com/example/demo/error/ErrorCode.java
@@ -0,0 +1,32 @@
+package com.example.demo.error;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+
+	CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "content not found"),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "user not found"),
+	LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "not liked yet"),
+	
+	USERNAME_CONFLICT(HttpStatus.CONFLICT, "username conflict"),
+	LIKE_CONFLICT(HttpStatus.CONFLICT, "aleady liked"),
+	
+	INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "check username or password"),
+	AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "please login"),
+	
+	NOT_CONTENT_OWNER(HttpStatus.FORBIDDEN, "not content owner"),
+	INCORRECT_PASSWORD(HttpStatus.FORBIDDEN, "incorrect password"),
+	USER_NOT_SAME(HttpStatus.FORBIDDEN, "user not same"),
+	
+	INVALID_IMAGE_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "invalid image file type"),
+	
+	PASSWORD_REQUIRED(HttpStatus.BAD_REQUEST, "password is required");
+	
+	private final HttpStatus status;
+	private final String message;
+}
